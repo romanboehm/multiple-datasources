23289217a0440eb8a12ca73aa8245cd3bb523c46
Roman Böhm <romanboehm@posteo.de>
2022-08-13 11:21:43 +0200

    Initial commit

---
+++ src/main/java/com/romanboehm/multipledatasources/MultipleDatasourcesApplication.java
@@ -0,0 +1,13 @@
+package com.romanboehm.multipledatasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDatasourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDatasourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romanboehm/multipledatasources/MultipleDatasourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romanboehm.multipledatasources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDatasourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
